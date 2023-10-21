9c46a7310088b7843209827439e091c255ad5765
Kader <kaderqayed23@gmail.com>
2023-10-21 15:00:10 -0400

    Finalized basic CGPA calculator

---
+++ main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int numClasses;
+    string letter;
+    double credits;
+    cout << "GPA Calculator" << endl;
+    cout << "______________" << endl;
+    cout << "Enter the number of classes:   ";
+    cin >> numClasses;
+
+
+    double grade = 0; //initialize grade and total credits.
+    double totalcredits = 0;
+
+    for(int i = 0; i < numClasses; i++){
+            // take input letter and credits.
+            cout << "Enter grade letter: ";
+            cin >> letter;
+            cout << "Enter number of credits: ";
+            cin >> credits;
+            cout << endl;
+
+            //grading sca;e
+            if (letter == "A") {
+                grade += (4.0 * credits);
+            } else if (letter == "B") {
+                grade += (3.0 * credits);
+            } else if (letter == "C") {
+                grade += (2.0 * credits);
+            } else if (letter == "D") {
+                grade += (1.0 * credits);
+            } else if (letter == "E") {
+                grade += (0.0 * credits);
+            }
+
+
+            totalcredits += credits;
+    }
+
+    //GPA calculation
+    double finalgrade = grade/totalcredits;
+
+    cout << "GPA: " << finalgrade << endl;
+
+
+
+}
